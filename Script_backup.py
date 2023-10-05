9b01d60683e75674736f53916b0200bde7ae2336
isupovGitKraken <viktorisup@gmail.com>
2023-10-05 17:14:48 +0300

    commit2

--- Script_backup.py
+++ Script_backup.py
@@ -3,9 +3,12 @@ import shutil
 import time
 
 
+# r'C:\ubn\id_rsa.txt'
+# r'C:\ubn\id_rsa1'
+
 prefix_name = time.strftime("%Y-%m-%d-%S")
-path_src = os.path.abspath(r'C:\ubn\id_rsa')
-path_dst = os.path.abspath(r'C:\ubn\id_rsa1')
+path_src = os.path.abspath(input('Введите путь источник'))
+path_dst = os.path.abspath(input('Введите путь назначения'))
 get_name_src = os.path.basename(path_src)
 create_copy_name = get_name_src + '_' + prefix_name
 create_path_dst = os.path.join(path_dst, create_copy_name)
@@ -19,7 +22,11 @@ exists_src_is_file = os.path.isfile(path_src)
 if exists_dst is False:
     os.makedirs(path_dst, mode=0o777, exist_ok=True)
 
-shutil.copy2(path_src, create_path_dst)
+try:
+    # if exists_src_is_file is True:
+    shutil.copy2(path_src, create_path_dst)
+except FileNotFoundError:
+    print('Не верный путь или имя файла')
 # while True:
 #     shutil.copy2(path_src, path_dst)
 #     time.sleep(30)

c1873a4229e39adf64db6633bcd831c40e90d9fa
isupovGitKraken <viktorisup@gmail.com>
2023-10-11 14:33:43 +0300

    commit4

--- pysmb.py
+++ pysmb.py
@@ -1,24 +1,24 @@
-import os
-import time
-from smb.SMBConnection import SMBConnection
-from smb.smb_structs import OperationFailure
-
-username = 'ivc'
-password = 'Mgmsu495!'
-server_ip = '192.168.6.5'
-port = 139
-local_dir = os.path.abspath(r'C:\ubn\id_rsa1')
-remote_dir = r"\Backup\test"
-
-
-class MySMB:
-
-    def __init__(self, username, password, server_ip, port):
-        self.samba = SMBConnection(
-            username, password, '', '', use_ntlm_v2=True)
-        self.samba.connect(server_ip, port)
-        self.service_name = "Disk1share"
-
+# # import os
+# # import time
+# # from smb.SMBConnection import SMBConnection
+# # from smb.smb_structs import OperationFailure
+# #
+# # username = 'ivc'
+# # password = 'Mgmsu495!'
+# # server_ip = '192.168.6.5'
+# # port = 139
+# # local_dir = os.path.abspath(r'C:\ubn\id_rsa1')
+# # remote_dir = r"\Backup\test"
+# #
+# #
+# # class MySMB:
+# #
+# #     def __init__(self, username, password, server_ip, port):
+# #         self.samba = SMBConnection(
+# #             username, password, '', '', use_ntlm_v2=True)
+# #         self.samba.connect(server_ip, port)
+# #         self.service_name = "Disk1share"
+# #
     def upload_files(self, local_dir, smb_base_dir: str):
         for root, dir_list, file_list in os.walk(local_dir):
             self.samba.createDirectory(self.service_name, smb_base_dir)
@@ -37,12 +37,47 @@ class MySMB:
                 with open(local_path, 'rb') as f:
                     self.samba.storeFile(
                         self.service_name, smb_path, f, timeout=3000)
-
-
-smb_obj = MySMB(username, password, server_ip, port)
-try:
-    smb_obj.upload_files(local_dir, remote_dir)
-except Exception as e:
-    print(e)
-finally:
-    smb_obj.samba.close()
+# #
+# #
+# # smb_obj = MySMB(username, password, server_ip, port)
+# # try:
+# #     smb_obj.upload_files(local_dir, remote_dir)
+# # except Exception as e:
+# #     print(e)
+# # finally:
+# #     smb_obj.samba.close()
+#
+# import os
+# from smb.SMBConnection import SMBConnection
+#
+#
+# def copy_folder_to_smb(local_folder_path, remote_folder_path, server_ip, username, password, client_name, server_name,
+#                        share_name):
+#     conn = SMBConnection(username, password, client_name, server_name, use_ntlm_v2=True)
+#     conn.connect(server_ip, 445)
+#     print(conn.isUsingSMB2)
+#
+#     for root, dirs, files in os.walk(local_folder_path):
+#         for file in files:
+#             local_file_path = os.path.join(root, file)
+#             remote_file_path = os.path.join(remote_folder_path, file)
+#
+#             with open(local_file_path, 'rb') as local_file:
+#                 conn.storeFile(share_name, remote_file_path, local_file)
+#
+#     print("Папка успешно скопирована на SMB-сервер!")
+#     conn.close()
+#
+#
+# # Пример использования функции
+# local_folder_path = 'C:/ubn/'
+# remote_folder_path = '/test/'
+# server_ip = '192.168.6.5'
+# username = 'ivc'
+# password = 'Mgmsu495!'
+# client_name = 'msmsu-isupov64'
+# server_name = 'msmsu-smb'
+# share_name = 'IVC-Doc'
+#
+# copy_folder_to_smb(local_folder_path, remote_folder_path, server_ip, username, password, client_name, server_name,
+#                    share_name)
--- smb_copy.py
+++ smb_copy.py
@@ -1,21 +1,58 @@
 import os
 import time
 from smb.SMBConnection import SMBConnection
-from smb.smb_structs import OperationFailure
 
-username = 'ivc'
-password = 'Mgmsu495!'
-server_ip = '192.168.6.5'
-port = 139
-local_dir = os.path.abspath(r'C:\ubn\id_rsa1')
-remote_dir = r"\Backup\test"
-f = open('tes1.txt', 'w+')
-samba = SMBConnection(username, password, '', '', use_ntlm_v2=True)
-samba.connect(server_ip, port)
-file_obj1 = samba.retrieveFile('Soft', '/test.txt', 'f')
-# print(file_obj)
-# content = samba.retrieveFile('Soft', '/')
-# if content:
-#     print(content.decode('utf-8'))  # декодируем содержимое файла в строку
-# else:
-#     print('Файл не найден')
+
+class MySMB:
+
+    def __init__(self, username, password, server_ip, port, local_dir, smb_base_dir, smb_dir):
+        self.backup_folder = time.strftime("%Y-%m-%d-%S")
+        self.local_dir = os.path.abspath(local_dir)
+        self.smb_base_dir = smb_base_dir
+        self.smb_dir = os.path.join(smb_dir, self.backup_folder)
+        self.name_file = os.path.basename(local_dir)
+        self.samba = SMBConnection(username, password, '', '', use_ntlm_v2=True)
+        self.samba.connect(server_ip, port)
+
+    def upload_files(self):
+        self.samba.createDirectory(self.smb_base_dir, self.smb_dir)
+        self.smb_dir = os.path.join(self.smb_dir, self.name_file)
+        with open(self.local_dir, 'rb') as file:
+            self.samba.storeFile(self.smb_base_dir, self.smb_dir, file)
+            print("Файл успешно скопирован на SMB-сервер!")
+
+    def upload_folder(self):
+        # self.samba.createDirectory(self.smb_base_dir, self.smb_dir)
+        for root, dir_list, file_list in os.walk(self.local_dir):
+            # for i in root:
+            walk_dir_tmp = root.split(self.local_dir)[1]
+            walk_dir_tmp = walk_dir_tmp[1:]
+            walk_dir = os.path.join(self.smb_dir, walk_dir_tmp)
+            print(walk_dir)
+            self.samba.createDirectory(self.smb_base_dir, walk_dir)
+
+
+login = 'ivc'
+passwd = 'Mgmsu495!'
+srv_ip = '192.168.6.5'
+prt = 139
+loc_dir = r'C:\ubn'
+smb_root_dir = 'Soft'
+smb_dir_after_root = r'\test'
+
+
+# for root, dir, file in os.walk(loc_dir):
+#     print(root)
+
+if __name__ == "__main__":
+    smb_obj = MySMB(login, passwd, srv_ip, prt, loc_dir, smb_root_dir, smb_dir_after_root)
+    try:
+        smb_obj.upload_folder()
+    except Exception as e:
+        print(e)
+    finally:
+        smb_obj.samba.close()
+
+
+
+

dc1506effafd76fb8c0a616150aa53da04eb696c
Viktor-home <v.isupov@mail.ru>
2023-10-12 01:02:37 +0300

    Реализовано копирование на SMB

--- smb_copy.py
+++ smb_copy.py
@@ -6,9 +6,10 @@ from smb.SMBConnection import SMBConnection
 class MySMB:
 
     def __init__(self, username, password, server_ip, port, local_dir, smb_base_dir, smb_dir):
-        self.backup_folder = time.strftime("%Y-%m-%d-%S")
+        self.backup_folder = 'Backup_' + os.path.basename(local_dir) + '_' + time.strftime("%Y-%m-%d-%S")
         self.local_dir = os.path.abspath(local_dir)
         self.smb_base_dir = smb_base_dir
+        self.smb_dir_src = smb_dir
         self.smb_dir = os.path.join(smb_dir, self.backup_folder)
         self.name_file = os.path.basename(local_dir)
         self.samba = SMBConnection(username, password, '', '', use_ntlm_v2=True)
@@ -24,21 +25,29 @@ class MySMB:
     def upload_folder(self):
         # self.samba.createDirectory(self.smb_base_dir, self.smb_dir)
         for root, dir_list, file_list in os.walk(self.local_dir):
-            # for i in root:
             walk_dir_tmp = root.split(self.local_dir)[1]
             walk_dir_tmp = walk_dir_tmp[1:]
             walk_dir = os.path.join(self.smb_dir, walk_dir_tmp)
-            print(walk_dir)
             self.samba.createDirectory(self.smb_base_dir, walk_dir)
+            for i in file_list:
+                item = os.path.join(root, i)
+                item_smb = item.split(self.local_dir)[1]
+                item_smb_join = os.path.join(self.backup_folder, item_smb[1:])
+                with open(item, 'rb') as file:
+                    if self.smb_dir_src == '':
+                        self.samba.storeFile(self.smb_base_dir, item_smb_join, file)
+                    else:
+                        self.samba.storeFile(self.smb_base_dir, os.path.join(self.smb_dir_src, item_smb_join), file)
+        print("Папка успешно скопирован на SMB-сервер!")
 
 
 login = 'ivc'
-passwd = 'Mgmsu495!'
-srv_ip = '192.168.6.5'
+passwd = '12345678'
+srv_ip = '192.168.55.4'
 prt = 139
 loc_dir = r'C:\ubn'
-smb_root_dir = 'Soft'
-smb_dir_after_root = r'\test'
+smb_root_dir = 'ivc folders'
+smb_dir_after_root = r'\test\test2'
 
 
 # for root, dir, file in os.walk(loc_dir):

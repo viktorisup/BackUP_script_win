58f4a3380fae9ac242b8ce7769607cab8b76519f
Viktor-home <v.isupov@mail.ru>
2023-10-14 01:01:02 +0300

    функция удаления папок

--- smb_copy.py
+++ smb_copy.py
@@ -7,7 +7,7 @@ from read_configuration import read_conf
 class MySMB:
 
     def __init__(self, username, password, server_ip, port, remote, domain, local_dir, smb_base_dir, smb_dir):
-        self.backup_folder = 'Backup_' + os.path.basename(local_dir) + '_' + time.strftime("%Y-%m-%d")
+        self.backup_folder = 'Backup_' + os.path.basename(local_dir) + '_' + time.strftime("%Y-%m-%d-%S")
         self.local_dir = os.path.abspath(local_dir)
         self.smb_base_dir = smb_base_dir
         self.smb_dir_src = smb_dir
@@ -17,6 +17,7 @@ class MySMB:
         self.samba.connect(server_ip, port)
 
     def upload_files(self):
+        print('Создаю копию файла')
         self.samba.createDirectory(self.smb_base_dir, self.smb_dir)
         self.smb_dir = os.path.join(self.smb_dir, self.name_file)
         with open(self.local_dir, 'rb') as file:
@@ -24,6 +25,7 @@ class MySMB:
             print("Файл успешно скопирован на SMB-сервер!")
 
     def upload_folder(self):
+        print('Создаю копию папки')
         for root, dir_list, file_list in os.walk(self.local_dir):
             walk_dir_tmp = root.split(self.local_dir)[1]
             walk_dir_tmp = walk_dir_tmp[1:]
@@ -40,10 +42,20 @@ class MySMB:
                         self.samba.storeFile(self.smb_base_dir, os.path.join(self.smb_dir_src, item_smb_join), file)
         print("Папка успешно скопирован на SMB-сервер!")
 
-    def delit_folder(self):
-        self.samba.deleteDirectory(self.smb_base_dir, self.smb_dir)
+    def delete_folder(self):
+        print('Удаляю старые бэкапы')
+        smb_list_path = self.samba.listPath(self.smb_base_dir, self.smb_dir_src)
+        smb_list_path = smb_list_path[2:]
+        now = time.time()
+        three_day = float(259200)
+        for i in smb_list_path:
+            if i.create_time < (now - three_day) and 'Backup' in i.filename:
+                self.samba.deleteFiles(self.smb_base_dir, os.path.join(self.smb_dir_src, i.filename, '*'),
+                                       delete_matching_folders=True)
+                self.samba.deleteDirectory(self.smb_base_dir, os.path.join(self.smb_dir_src, i.filename))
+        print('Бэкапы успешно удалены')
 
-
+#259200
 if __name__ == "__main__":
 
     cfg_dict = read_conf()
@@ -74,8 +86,10 @@ if __name__ == "__main__":
     try:
         if os.path.isfile(loc_dir_c) is True:
             smb_obj.upload_files()
+            smb_obj.delete_folder()
         else:
-            smb_obj.delit_folder()
+            smb_obj.upload_folder()
+            smb_obj.delete_folder()
     except Exception as e:
         print(e)
     finally:
